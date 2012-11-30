1922add34866fd67bfba957f310b174b909ebfcd
Rob Mensching <rob@robmensching.com>
2012-11-30 01:34:09 -0800

    Introducing Simplified WiX Toolset.

---
+++ src/swix/swcore/AssemblyInfo.cs
@@ -0,0 +1,34 @@
+﻿//-------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Outercurve Foundation">
+//   Copyright (c) 2004, Outercurve Foundation.
+//   This software is released under Microsoft Reciprocal License (MS-RL).
+//   The license and further copyright text can be found in the file
+//   LICENSE.TXT at the root directory of the distribution.
+// </copyright>
+//-------------------------------------------------------------------------------------------------
+
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Windows.Markup;
+
+[assembly: AssemblyTitle("Simplified WiX Toolset Core")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyCulture("")]
+[assembly: CLSCompliant(true)]
+[assembly: ComVisible(false)]
+
+// Map XML namespaces to prefixes to CLR namespaces.
+[assembly: XmlnsPrefix("http://wixtoolset.org/schemas/2010/wix/simplified", "")]
+[assembly: XmlnsPrefix("http://wixtoolset.org/schemas/2010/wix/simplified/appx", "appx")]
+[assembly: XmlnsPrefix("http://wixtoolset.org/schemas/2010/wix/simplified/msi", "msi")]
+[assembly: XmlnsPrefix("http://wixtoolset.org/schemas/2010/wix/simplified/vsix", "vsix")]
+
+[assembly: XmlnsDefinition("http://wixtoolset.org/schemas/2010/wix/simplified", "WixToolset.Simplified.Lexicon")]
+[assembly: XmlnsDefinition("http://wixtoolset.org/schemas/2010/wix/simplified/appx", "WixToolset.Simplified.Lexicon.Appx")]
+[assembly: XmlnsDefinition("http://wixtoolset.org/schemas/2010/wix/simplified/msi", "WixToolset.Simplified.Lexicon.Msi")]
+[assembly: XmlnsDefinition("http://wixtoolset.org/schemas/2010/wix/simplified/vsix", "WixToolset.Simplified.Lexicon.Vsix")]
+
+// Expose our internal types to the unit test assembly.
+//[assembly: InternalsVisibleTo("WixToolset.Simplified.UnitTest.Swcore")]
---
+++ src/swix/unittests/swcoreTests/AssemblyInfo.cs
@@ -0,0 +1,45 @@
+﻿//-------------------------------------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="Outercurve Foundation">
+//   Copyright (c) 2004, Outercurve Foundation.
+//   This software is released under Microsoft Reciprocal License (MS-RL).
+//   The license and further copyright text can be found in the file
+//   LICENSE.TXT at the root directory of the distribution.
+// </copyright>
+//-------------------------------------------------------------------------------------------------
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("swcoreTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("swcoreTests")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("851d694d-a02c-4b83-bcc5-1237bd5e7a53")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
